8ad6d607ff849538fe5db3509b5ac69660bc7c18
UmutMdn <111302020+UmutMdn@users.noreply.github.com>
2023-05-19 08:03:06 +0300

    MagzaSınıf
    Sınıf oluşturma

---
+++ ProgramlamOdev3/magazaOdevi.py
@@ -0,0 +1,43 @@
+class Magaza:
+    def __init__(self, magaza_adi, satici_adi, satici_cinsi):
+        self.__magaza_adi = magaza_adi
+        self.__satici_adi = satici_adi
+        self.__satici_cinsi = satici_cinsi
+        self.__satis_tutari = 0
+
+    def set_magaza_adi(self, magaza_adi):
+        self.__magaza_adi = magaza_adi
+
+    def get_magaza_adi(self):
+        return self.__magaza_adi
+
+    def set_satici_adi(self, satici_adi):
+        self.__satici_adi = satici_adi
+
+    def get_satici_adi(self):
+        return self.__satici_adi
+
+    def set_satici_cinsi(self, satici_cinsi):
+        self.__satici_cinsi = satici_cinsi
+
+    def get_satici_cinsi(self):
+        return self.__satici_cinsi
+
+    def set_satis_tutari(self, satis_tutari):
+        self.__satis_tutari = satis_tutari
+
+    def get_satis_tutari(self):
+        return self.__satis_tutari
+
+
+
+
+
+
+
+
+
+
+
+
+
